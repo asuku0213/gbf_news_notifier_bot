e26bf5bfeef2cd51352f4f129f877a2c04738960
asuku0213 <39723622+asuku0213@users.noreply.github.com>
2019-07-05 01:35:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   discord: {
     token: 'NTk2NTY0Mjk3NzQ0Nzc3MjE2.XR7hCQ.gC-1bx0aCBEfnppsmJUJGXWxSh4',
-    targetChannelName: 'general'
+    targetChannelName: 'gbf-news'
   },
   network: {
     checkIntervalMs: 10 * 60 * 1000
